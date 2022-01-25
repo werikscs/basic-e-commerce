909aec0a73b2c075899ab6e31356535b19a4cd3e
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-01-25 17:32:51 -0300

    refatora layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,49 @@
 setupMenuSelection();
+setupCartVisibilityAtResize();
 setupFloatCartButton();
 
-function setupFloatCartButton() {
 
-	const cartFloatButton = document.getElementById('cart-floatBtn');
-	const closeCartButton = document.getElementById('cart-btnMobile-closeCart');
+function setupCartVisibilityAtResize() {
 
 	const cart = document.getElementById('cart');
 	const vitrine = document.getElementById('vitrine');
+
+	const cartFloatButton = document.getElementById('cart-floatBtn');
+	const closeCartButton = document.getElementById('cart-btnMobile-closeCart');
 
 	const cartWithItems = document.getElementById('cart-withItems');
 	const cartWithoutItems = document.getElementById('cart-withoutItems');
 	const cartPurchaseInfo = document.getElementById('cart-purchaseInfo');
 
+	//se começar no modo desktop, aparece o carrinho vazio por padrão
+	if (innerWidth >= 1125) {
+		cart.classList.remove('closed');
+	}
+
+	//se modo desktop, aparece carrinho
+	//se modo mobile, carrinho não começa aberto
+	window.addEventListener('resize', function () {
+		if (innerWidth < 1125) {
+			cart.classList.add('closed');
+		}
+
+		if (innerWidth >= 1125) {
+			cart.classList.remove('closed');
+		}
+	});
+
+}
+
+function setupFloatCartButton() {
+	const cart = document.getElementById('cart');
+	const vitrine = document.getElementById('vitrine');
+
+	const cartFloatButton = document.getElementById('cart-floatBtn');
+	const closeCartButton = document.getElementById('cart-btnMobile-closeCart');
+
+	cartFloatButton.addEventListener('click', function () {
+		cart.classList.remove('closed');
+	});
 }
 
 function setupMenuSelection() {
