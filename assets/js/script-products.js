94e683fea1b6ba2fc5ced74fb5f9ba9c8bd667c2
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-01-25 19:47:32 -0300

    adiciona obj products

---
+++ assets/js/script-products.js
@@ -0,0 +1,49 @@
+const products = [
+	{
+		'img': '/assets/img/lightweight-jacket.png',
+		'categoryTag': 'camisetas',
+		'name': 'Lightweight Jacket',
+		'description': 'Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...',
+		'price': '100.00'
+	},
+
+	{
+		'img': '/assets/img/black-hat.png',
+		'categoryTag': 'acessorios',
+		'name': 'Black Hat',
+		'description': 'O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...',
+		'price': '100.00'
+	},
+
+	{
+		'img': '/assets/img/mask.png',
+		'categoryTag': 'acessorios',
+		'name': 'Mask',
+		'description': 'Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...',
+		'price': '40.00'
+	},
+
+	{
+		'img': '/assets/img/t-shirt.png',
+		'categoryTag': 'camisetas',
+		'name': 'T-Shirt',
+		'description': 'Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...',
+		'price': '100.00'
+	},
+
+	{
+		'img': '/assets/img/short-sleeve-t-thirt.png',
+		'categoryTag': 'camisetas',
+		'name': 'Short-Sleeve T-Shirt',
+		'description': 'Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...',
+		'price': '100.00'
+	},
+
+	{
+		'img': '/assets/img/champion-packable-jacket.png',
+		'categoryTag': 'camisetas',
+		'name': 'Champion Packable Jacket',
+		'description': 'Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...',
+		'price': '100.00'
+	},
+]
